e5ed4daaf1f338c21695735aaa3813d884c14a69
Rod McNew <rmcnew@relivinc.com>
2015-10-20 17:14:12 -0500

    Update rcm-file-chooser-factory.example.js

--- rcm-file-chooser-factory.example.js
+++ rcm-file-chooser-factory.example.js
@@ -1,4 +1,4 @@
 window.rcmFileChooser = new S3ExplorerAdaptor(
     'PUT_YOUR_BUCKET_NAME_HERE',
-    '/PUT_SOMETHING_HERE/open-source-able/code-from-aws/index.html'
+    '/PUT_SOMETHING_HERE/code-from-aws/index.html'
 );
